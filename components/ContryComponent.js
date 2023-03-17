71fdd4a99f5b185a01b1afbe08d4b17776c9ca50
hak <hak404notfound@gmail.com>
2023-03-18 00:24:15 +0700

    progress pagination

--- components/ContryComponent.js
+++ components/ContryComponent.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect } from 'react'
 import axios from "axios";
-import { Button, Modal } from 'antd';
+import { Button, Modal, Alert, Space, Spin } from 'antd';
 
 const ContryComponent = () => {
 
     const [countries, setCountries] = useState([]);
     const [country, setCountry] = useState({});
     const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [countryperPage, setCountryPerPage] = useState(25);
 
     const fetchCountry = async () => {
         const { data } = await axios.get(`https://restcountries.com/v3.1/all`);
@@ -19,25 +22,36 @@ const ContryComponent = () => {
 
     const findOneCountry = async (name) => {
         if(name) {
-            setOpen(true);
+            setLoading(true);
             const { data } = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
-            setCountry(data);
+            if (data) {
+                console.warn(data[0]);
+                setCountry(data[0]);
+                setLoading(false);
+                setOpen(true);
+            }
         }
     }
 
+    const lastIndex = currentPage * countryperPage;
+    const firstIndex = lastIndex - countryperPage;
+    const currentCountry = countries.slice(firstIndex, lastIndex);
+
   return (
       <>
-          <Modal
-              title="View more detail"
-              centered
-              open={open}
-              onOk={() => setOpen(false)}
-              onCancel={() => setOpen(false)}
-              width={900}
-          >
-              <p>{country.name?.official}</p>
-              <p>some contents...</p>
-              <p>some contents...</p>
+          <Modal title="View more detail" style={{ top: 100 }} open={open} onOk={() => setOpen(false)} onCancel={() => setOpen(false)} width={900}>
+              <div className="container">
+                  <div className="row">
+                        <div className="col-lg-5">
+                          <div className="card">
+                              <img src={`${country.flags?.png}`} className="card-img-top" width="100%" height="100%" alt="..." />
+                          </div>      
+                        </div>
+                        <div className="col-lg-7">
+                          <h4>{country.name?.official}</h4>
+                        </div>
+                    </div>
+                </div>
           </Modal>
 
           <div className="album py-5 bg-light">
@@ -49,8 +63,8 @@ const ContryComponent = () => {
                                     <div className="card shadow-sm">
                                         <img src={`${item.flags.png}`} className="bd-placeholder-img card-img-top" width="100%" height="100%" alt="Country" />
                                         <div className="card-body">
-                                            <p className="card-text">{item.name.official}</p>
-                                            <p className="card-text">{item.name?.nativeName?.zho?.official}</p>
+                                            <p className="card-text">{item.name?.official}</p>
+                                          <p className="card-text">{item.idd?.root}{item.idd?.suffixes}</p>
                                             <div className="d-flex justify-content-between align-items-center">
                                                 <div className="btn-group">
                                                     <Button type="primary" onClick={() => findOneCountry(item.name.official)}>
